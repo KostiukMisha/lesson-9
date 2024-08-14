a2061cee08ba35c2645ca98ff87bc540c315d81e
Kostiuk Misha <kostiukmisha2001@gmail.com>
2024-08-14 09:54:53 +0300

    lesson 9

---
+++ lesson9.2.py
@@ -0,0 +1,79 @@
+import random
+random_number = random.randint(1,100)
+your_number = float(input("ggues number1: "))
+if your_number > random_number:
+    print("меньше")
+elif your_number < random_number:
+    print("больше")
+elif your_number == random_number:
+    print("выйграл")
+    exit()    
+your_number = float(input("ggues number2: "))
+if your_number > random_number:
+    print("меньше")
+elif your_number < random_number:
+    print("больше")
+elif your_number == random_number:
+    print("выйграл")
+    exit() 
+your_number = float(input("ggues number3: "))
+if your_number > random_number:
+    print("меньше")
+elif your_number < random_number:
+    print("больше")
+elif your_number == random_number:
+    print("выйграл")
+    exit() 
+your_number = float(input("ggues number4: "))
+if your_number > random_number:
+    print("меньше")
+elif your_number < random_number:
+    print("больше")
+elif your_number == random_number:
+    print("выйграл")
+    exit() 
+your_number = float(input("ggues number5: "))
+if your_number > random_number:
+    print("меньше")
+elif your_number < random_number:
+    print("больше")
+elif your_number == random_number:
+    print("выйграл")
+    exit() 
+your_number = float(input("ggues number6: "))
+if your_number > random_number:
+    print("меньше")
+elif your_number < random_number:
+    print("больше")
+elif your_number == random_number:
+    print("выйграл")
+    exit() 
+your_number = float(input("ggues number7: "))
+if your_number > random_number:
+    print("меньше")
+elif your_number < random_number:
+    print("больше")
+elif your_number == random_number:
+    print("выйграл")
+    exit() 
+your_number = float(input("ggues number8: "))
+if your_number > random_number:
+    print("меньше")
+elif your_number < random_number:
+    print("больше")
+elif your_number == random_number:
+    print("выйграл")
+    exit() 
+your_number = float(input("ggues number9: "))
+if your_number > random_number:
+    print("меньше")
+elif your_number < random_number:
+    print("больше")
+elif your_number == random_number:
+    print("выйграл")
+    exit() 
+your_number = float(input("ggues number10: "))
+if your_number == random_number:
+    print("выйграл")
+print("вы проиграли")
+exit() 
